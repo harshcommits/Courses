514de9472046a2d0449bd410f5333700782d0b4d
Harsh Jha <iamharsh711@gmail.com>
2022-01-22 00:51:17 +0530

    added microservices course code

--- Linkedin Learning/Full Stack Apps with React and Spring/project/src/main/java/model/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface ContactRepository extends CrudRepository<Contact, Long> {
-
-}
