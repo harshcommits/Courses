dc80752db50601a45f556e88ca72b8db08d0bfce
Harsh Jha <jhaharsh96@gmail.com>
2022-02-07 09:54:30 +0530

    updated DS files for python

--- Linkedin Learning/Python Courses/Python DS/balance_symbols.py
+++ Linkedin Learning/Python Courses/Python DS/balance_symbols.py
@@ -7,16 +7,47 @@ class Stack:
         self.items.append(item)
 
     def pop(self):
-        if self.items:
-            return self.items.pop()
-        else:
-            return None
-
-    def peek(self):
-        return self.items[-1]
-
-    def size(self):
-        return len(self.items)
+        return self.items.pop()
 
     def is_empty(self):
         return self.items == []
+
+
+def match_symbols(str_example):
+
+    symbol_pairs = {
+        '(': ')',
+        '[': ']',
+        '{': '}',
+    }
+
+    openers = symbol_pairs.keys()
+    mystack = Stack()
+
+    index = 0
+    while index < len(str_example):
+        symbol = str_example[index]
+
+        if symbol in openers:
+            mystack.push(symbol)
+        else:
+
+            if mystack.is_empty():
+                return False
+ 
+            else:
+                top_item = mystack.pop()
+                if symbol != symbol_pairs[top_item]:
+                    return False
+                    
+            print(mystack.printvalues())
+
+        index += 1
+
+    if mystack.is_empty():
+        return True
+
+    return False
+
+print(match_symbols('({[]})'))
+#print(match_symbols('({[]})}'))
