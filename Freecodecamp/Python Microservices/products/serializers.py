adfeadfcbb0dd8a0906bc5fdfc0f0c0d55f739cd
Harsh Jha <jhaharsh96@gmail.com>
2022-02-27 12:12:27 +0530

    added design patterns course files

--- Freecodecamp/Python Microservices/products/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-class ProductSerializer(serialize):
-    class Meta:
-        model = Product
-        fields = '__all__'
---
+++ Linkedin Learning/Python Courses/Python Design Patterns/Creational Patterns/prototype.py
@@ -0,0 +1,9 @@
+import copy
+
+class Prototype:
+
+     def __init__(self):
+          self._objects = {}
+
+     def register_object(self, name, obj):
+          pass
