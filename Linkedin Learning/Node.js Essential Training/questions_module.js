ec036871e372a44a1905fa5c5e825445287c9ec2
Harsh Jha <harshjha@Harshs-MacBook-Air.local>
2022-02-27 20:48:22 +0530

    even more cleanup

--- Linkedin Learning/Node.js Essential Training/questions_module.js
+++
@@ -1,7 +0,0 @@
-const collectAnswers = require("./lib/collectAnswers_module");
-
-const questions = [
-    "What is your name? ",
-    "Where do you live?",
-    "What are you going to do with Node.js?"
-];
---
+++ Linkedin Learning/Python Courses/Python Design Patterns/Behavioral Patterns/visitor.py
@@ -0,0 +1,25 @@
+class House(object):
+    def accept(self, visitor):
+        """Interface to accept a visitor"""
+        visitor.visit(self)
+
+    def work_on_hvac(self, hvac_specialist):
+        print(self, "worked on by", hvac_specialist)
+
+    def work_on_hvac(self, electrician):
+        print(self, "worked on by", electrician)
+
+    def __str__(self) -> str:
+        """return the class name when House object is printed"""
+        return self.__class__.__name__
+
+class Visitor(object):
+    """Abstract visitor"""
+    def __str__(self):
+        """Simply return class name when visitor object is printed"""
+        return self.__class__.__name__
+
+class HvacSpecialist(Visitor):
+    """Concrete visitor: HVAC specialist"""
+    def visit(self, house):
+        house.work_on_hvac(self)
