cd5634f9b7fe2f88dc69c2d25db0aa9229378d50
Harsh Jha <harshjha@Harshs-MacBook-Air.local>
2022-02-27 19:58:26 +0530

    added FCC iOS Project

--- Linkedin Learning/Python Courses/Python Design Patterns/Behavioral Patterns/observer.py
+++ Linkedin Learning/Python Courses/Python Design Patterns/Behavioral Patterns/observer.py
@@ -2,3 +2,12 @@ class Subject(object):
 
     def __init__(self):
         self._observers = []
+
+    def attach(self, observer): #add observer to observer's list if not already there
+        pass
+
+    def detach(self, observer):
+        try:
+            self._observers.remove(observer)
+        except ValueError:
+            pass
