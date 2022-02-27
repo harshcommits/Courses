56a961e2dee7f79858d59252a8ca63953c2ffc38
Harsh Jha <jhaharsh96@gmail.com>
2022-02-27 19:00:52 +0530

    cleanup old course code

--- Linkedin Learning/Learning Django/wisdompets/adoptions/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Pet
-
-@admin.register(Pet)  #this takes model class as argument
-class PetAdmin(admin.ModelAdmin):
-     list_display = ['name', 'species', 'breed', 'age', 'sex']
