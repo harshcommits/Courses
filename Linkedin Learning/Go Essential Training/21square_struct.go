72a30f36152c0cbe41a3c477f0d51e39cdbb47ce
Harsh Jha <harsh@example.com>
2022-03-12 17:45:34 +0530

    updated go files

---
+++ Linkedin Learning/Go Essential Training/21square_struct.go
@@ -0,0 +1,18 @@
+package main
+
+/*
+define a square struct with 2 fields; center of type point and length of type int
+define following methods:
+	1. Move(dx int, dy int)
+	2. Area() int
+also has following new square definition;
+	NewSquare(x int, y int, length int) (*Square, error)
+*/
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("this is a placeholder till we write actual ")
+}
