1f069490ae9d0080aae32e66d7b5cc5e4478fb6e
Harsh Jha <harshjha@Harshs-MacBook-Air.local>
2022-03-12 23:34:19 +0530

    added code for logging

--- Linkedin Learning/Go Essential Training/24package_error.go
+++ Linkedin Learning/Go Essential Training/24package_error.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
-
-	"github.com/pkg/errors"
 )
 
 type Config struct {
@@ -13,8 +12,8 @@ type Config struct {
 func readConfig(path string) (*Config, error) {
 	file, err := os.Open(path)
 	if err != nil {
-		//return nil, err
-		return nil, errors.Wrap(err, "Can't open config file")
+		return nil, err
+		//return nil, errors.Wrap(err, "Can't open config file")
 	}
 
 	defer file.Close()
@@ -23,10 +22,21 @@ func readConfig(path string) (*Config, error) {
 	return cfg, nil
 }
 
+func setupLogging() {
+	out, err := os.OpenFile("app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644) //creates app.log with error
+	if err != nil {
+		return
+	}
+
+	log.SetOutput(out)
+}
+
 func main() {
+	setupLogging()
 	cfg, err := readConfig("/path/to/config.file")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+		log.Printf("error: %+v\n", err)
 		os.Exit(1)
 	}
 
