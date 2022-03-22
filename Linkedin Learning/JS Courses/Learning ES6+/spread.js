592963374befe68171533dc484f08d2fcbc4eb17
Harsh Jha <harshjha@Harshs-MacBook-Air.local>
2022-03-22 17:46:53 +0530

    restructured code

--- Linkedin Learning/JS Courses/Learning ES6+/spread.js
+++
@@ -1,8 +0,0 @@
-// ... - spread operator
-
-let cats = ["biscuits", "jungle"]
-let dogs = ["Stella", "Casper"]
-
-let animals = ["Smokey", "Miro", "Swimmy", ...cats, ...dogs]
-
-console.log(animals)
