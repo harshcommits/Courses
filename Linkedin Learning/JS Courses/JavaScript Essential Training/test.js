6f3b469ae5148d2fa3701a391a34814774533b59
Harsh Jha <harshjha@Harshs-MacBook-Air.local>
2022-03-23 12:27:10 +0530

    added new code

--- Linkedin Learning/JS Courses/JavaScript Essential Training/test.js
+++ Linkedin Learning/JS Courses/JavaScript Essential Training/test.js
@@ -1,1 +1,11 @@
 console.log("just a placeholder to test the JS script tag");
+
+const backpack = {
+    color: "Blue",
+    size: "L",
+    cost: 200,
+    open: false
+}
+
+console.log(backpack.color);
+console.log(backpack['cost']);
