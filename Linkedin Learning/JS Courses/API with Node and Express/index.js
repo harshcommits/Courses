d79bcf20dc2c97865101d54c986727313213ae4f
Harsh Jha <harshjha@Harshs-MacBook-Air.local>
2022-04-03 10:42:58 +0530

    updated changes

--- Linkedin Learning/JS Courses/API with Node and Express/index.js
+++ Linkedin Learning/JS Courses/API with Node and Express/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 
 const app = express();
-const port = 4000;
+const PORT = 4000;
 
 app.get('/', (req, res) => 
-    res.send(`Node and express server running on port ${port}`)
+    res.send(`Node and express server running on port ${PORT}`)
 );
 
-app.listen(port, () => 
-    console.log(`Your server is running on port ${port}`)
+app.listen(PORT, () => 
+    console.log(`Your server is running on port ${PORT}`)
 );
--- Youtube/Freecodecamp/Intermediate Python Programming/set.py
+++
@@ -1,4 +0,0 @@
-myset = set("Hello")
-print(myset)
-
-#can perform all set operations; add,remove,union,intersection etc.
