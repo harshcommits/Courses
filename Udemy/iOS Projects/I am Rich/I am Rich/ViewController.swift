f92eafe51756216dbcd7dc9d675bada7950a2545
Harsh Jha <harshjha@Harshs-MacBook-Air.local>
2022-04-26 23:13:17 +0530

    fixes

---
+++ Udemy/iOS Projects/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Harsh Jha on 24/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
