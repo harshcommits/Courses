81469514cdd10220748b8fbe6010dab6ee4de02b
Harsh Jha <harshjha@Harshs-MacBook-Air.local>
2022-04-29 11:16:27 +0530

    final python OOP file

---
+++ Linkedin Learning/Python Courses/Python Object Oriented Programming/17immutable_class.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+@dataclass(frozen=True) #frozen=True makes class values immutable
+class ImmutableClass:
+    value1 : str = "Value 1"
+    value2 : int = 0
+
+    def somefunc(self, newval):
+        self.value2 = newval
+
+obj = ImmutableClass()
+print(obj.value1)
+
+#trying to change value
+# obj.value1 = "Another string" #gives frozeninstance error
+# print(obj.value1) 
+
+#even using class methods doesn't change values
+obj.somefunc(30) #gives same error as previous
