21d02f18e5d2f0338d0a020781b4d33b68dd0590
Harsh Jha <harsh@gmail.com>
2022-06-06 23:00:54 +0530

    syntax updates

--- Linkedin Learning/Python Courses/Python OOP/1class-definition.py
+++ Linkedin Learning/Python Courses/Python OOP/1class-definition.py
@@ -2,6 +2,7 @@ class Book:
     def __init__(self, title):
         self.title = title
 
+
 b1 = Book("Brave New World")
 print(b1.title)
 b2 = Book("War and Peace")
--- Linkedin Learning/Python Courses/Python OOP/3type_check.py
+++ Linkedin Learning/Python Courses/Python OOP/3type_check.py
@@ -6,18 +6,19 @@ class Newspaper:
     def __init__(self, name):
         self.name = name
 
+
 b1 = Book("War and Peace")
 b2 = Book("Anna Karenina")
 n1 = Newspaper("The Washington Post")
 n2 = Newspaper("The New York Times")
 
-#get types
+# get types
 print(type(b1))
 print(type(n1))
 
-#compare types
+# compare types
 print(type(b1) == type(n1))
 
-#using isinstance
+# using isinstance
 print(isinstance(b1, Book))
 print(isinstance(b1, object))
