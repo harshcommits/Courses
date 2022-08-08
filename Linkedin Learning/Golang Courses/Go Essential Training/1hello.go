4942407766f6b2d9a7db3babd2aaf81bdf93f112
Harsh <harsh@Harshs-MacBook-Air.local>
2022-08-08 21:25:29 +0530

    updated files in course

--- Linkedin Learning/Golang Courses/Go Essential Training/1hello.go
+++ Linkedin Learning/Golang Courses/Go Essential Training/1hello.go
@@ -8,4 +8,13 @@ func main() {
 
 	fmt.Println("Hello there!")
 
+	//multi-line declaration
+	var data int32
+	data = 32
+	fmt.Printf("Multi-line declaration: %d\n", data)
+
+	//single-line declaration
+	other := 34
+	fmt.Printf("Single line declaration: %d", other)
+
 }
--- Linkedin Learning/Golang Courses/Learning Go/16methods.go
+++ Linkedin Learning/Golang Courses/Learning Go/16methods.go
@@ -22,11 +22,17 @@ func main() {
 
 }
 
-func (d Dog) Speak() { //d is receiver, where d is an instance of Dog struct
+func (d Dog) Speak() { //d is receiver, where d is an instance of Dog struct and can be called in the method itself
 	fmt.Println(d.Sound)
 }
 
 func (d Dog) SpeakThreeTimes() {
-	d.Sound = fmt.Sprintf("%v %v %v", d.Sound, d.Sound, d.Sound)
-	fmt.Println(d.Sound)
+	/*
+		d.Sound = fmt.Sprintf("%v %v %v", d.Sound, d.Sound, d.Sound)
+		fmt.Println(d.Sound)
+	*/
+
+	for i := 0; i < 3; i++ {
+		fmt.Println(d.Sound)
+	}
 }
