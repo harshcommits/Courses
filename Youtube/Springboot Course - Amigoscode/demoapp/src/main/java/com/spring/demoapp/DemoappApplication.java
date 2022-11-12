271bfc13fe46b982c656564b951492429abb1033
Harsh <harsh@Harshs-MacBook-Air.local>
2022-11-12 21:13:09 +0530

    more updates

--- Youtube/Springboot Course - Amigoscode/demoapp/src/main/java/com/spring/demoapp/DemoappApplication.java
+++ Youtube/Springboot Course - Amigoscode/demoapp/src/main/java/com/spring/demoapp/DemoappApplication.java
@@ -4,7 +4,6 @@ import com.spring.demoapp.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class DemoappApplication {
 
