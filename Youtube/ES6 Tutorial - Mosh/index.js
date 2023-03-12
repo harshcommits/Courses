20b69bf8489db8bbc4c8734fb082eca4b6accc13
Harsh <harsh@Harshs-MacBook-Air.local>
2023-03-12 23:46:54 +0530

    more folder renames

--- Youtube/ES6 Tutorial - Mosh/index.js
+++
@@ -1,8 +0,0 @@
-function runhello() {
-    for (let i = 0; i < 5; i++) {
-        console.log(i);
-    }
-    console.log("Outside the loop", i); //prints 5 when var used; fails when we use let
-}
-
-runhello();
