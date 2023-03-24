b9da4e931853fd9ed82ba3a099592af33f4333db
Harsh Jha <jhaharsh96@gmail.com>
2023-03-24 19:20:06 +0530

    added for concurrency

--- pluralsight/learning-nodejs-12/modules/arguments.js
+++ pluralsight/learning-nodejs-12/modules/arguments.js
@@ -4,7 +4,8 @@ function dynamicArgsFunctions() {
 
 dynamicArgsFunctions(1, 2, 3, 4);
 
-// the following line will print 5 arguments, even though there's nothing passed. Run the code to get an idea
+// the following line will print 5 arguments(only when using node, not in browser), even though there's nothing passed. Run the code to get an idea
 // they are; function(exports, module, require, __filename, __dirname)
+// read up on wrapper functions in JS
 
 console.log(arguments);
