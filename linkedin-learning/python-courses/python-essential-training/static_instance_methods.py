cdddf60cae138a8d79701f2a10c8efc80f9402cb
Harsh <jhaharsh96@gmail.com>
2024-05-20 00:04:08 +0530

    more updates

--- linkedin-learning/python-courses/python-essential-training/static_instance_methods.py
+++ linkedin-learning/python-courses/python-essential-training/static_instance_methods.py
@@ -1,15 +1,26 @@
+# need to read up on the @staticmethod decorator
+
 class WordList:
+    Puncs = [',', '.', '!', '\'']
     def __init__(self):
         self.words = set()
 
 
-    def addText(self, text):
-        text = self.clearText(text)
+    def addText(self, text):    # example of an instance method
+        text = WordList.clearText(text)
+        for word in text.split():
+            self.words.add(word)
 
-    def clearText(self, text):
+    def clearText(text):    # self keyword not required since this is a static method, and therefore linked to the class itself
         # chaining functions
-        text = text.replace('!', '').replace('.', '').replace(',', '').replace('\'', '')
+        # text = text.replace('!', '').replace('.', '').replace(',', '').replace('\'', '')
+        for punc in WordList.Puncs:
+            text = text.replace(punc, '')
+        return text.lower()
 
 if __name__ == "__main__":
 
     wordlist = WordList()
+    wordlist.addText("Hello, I am Harsh! Here is some text I want to test here.")
+
+    print(wordlist.words)
