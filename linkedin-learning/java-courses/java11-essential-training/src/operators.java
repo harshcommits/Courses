60057e54509ef44aad2537eeb2f6bf6804efdb51
Harsh Jha <jhaharsh96@gmail.com>
2024-10-20 13:05:46 +0530

    removed java project

--- linkedin-learning/java-courses/java11-essential-training/src/operators.java
+++
@@ -1,9 +0,0 @@
-public class operators {
-    public static void main(String[] args) {
-
-        var intValue = 10; //default value is 0
-        System.out.println(intValue++); //output: 10; new value: 11
-        System.out.println(++intValue); //output: 11; new value: 11
-
-    }
-}
